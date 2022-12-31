66ae6b9910e559e4a140618cbba222dab88694fb
Matias J. Apablaza <mapablaza@itba.edu.ar>
2022-12-31 17:37:11 -0300

    Challenge solution

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 new Vue({
   el: '#app',
   data: {
-    contacts: []
+    contacts: [],
   },
   methods: {
-    
+    toggleFavorite(contact) {
+      Vue.set(contact, 'favorite', !contact.favorite);
+    },
+
+    removeContact(contact) {
+      let confirm = window.confirm("Do you want to remove this contact?")
+      if(confirm)
+        this.contacts = this.contacts.filter(c => c !== contact);
+    },
+
+    async fetchData() {
+      try {
+        let res = await fetch('https://randomuser.me/api/?results=50')
+        res = await res.json()
+        this.contacts = res.results
+      } catch (e) {
+        alert("Couldn't fetch contacts")
+      }
+    }
   },
   mounted() {
-    
+    this.fetchData()
   }
 });
