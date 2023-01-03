164787411856dc8a5ed333d67b6a8deccdee5046
Leonardo D'Agostino <ldagostino@itba.edu.ar>
2023-01-03 13:02:10 -0300

    feat: favorite feature

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ new Vue({
       const RANDOMUSER_API = "https://randomuser.me/api/";
       return fetch(`${RANDOMUSER_API}?results=${resultsNumber}`);
     },
+
+    toggleFavorite(contact) {
+      contact.isFavorite = !contact.isFavorite;
+      console.log("contacts", this.contacts);
+    },
   },
   created() {
     this.getContacts(50)
       .then((contacts) => contacts.json())
       .then((contacts) => {
-        this.contacts = contacts.results;
+        this.contacts = contacts.results.map((contact) => {
+          return {
+            ...contact,
+            isFavorite: false
+          }
+        });
         console.log("contacts", this.contacts);
       })
       .catch((error) => {
